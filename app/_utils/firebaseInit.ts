9680a4df6c01705043eb630219bfca1cde90b64f
PlayXman <play.man.life@seznam.cz>
2025-02-25 23:10:15 +1100

    fix: remove hardcoded project keys

--- app/_utils/firebaseInit.ts
+++ app/_utils/firebaseInit.ts
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app";
 import {connectDatabaseEmulator, getDatabase} from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgs1D3EWtG5QP2egMmKEkPfsj2DOB1svg",
-  authDomain: "aa-gamescz-news.firebaseapp.com",
-  databaseURL: "https://aa-gamescz-news-default-rtdb.firebaseio.com",
-  projectId: "aa-gamescz-news",
-  storageBucket: "aa-gamescz-news.firebasestorage.app",
-  messagingSenderId: "629484238169",
-  appId: "1:629484238169:web:f3229d2347d6c23b670848"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
+
+console.log(firebaseConfig);
 
 initializeApp(firebaseConfig);
 
