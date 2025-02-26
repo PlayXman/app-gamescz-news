1bf57f7d58f9b6354f9375431def8841754550cc
PlayXman <play.man.life@seznam.cz>
2025-02-26 15:12:22 +1100

    fix: remove unnecessary logging

--- app/_utils/firebaseInit.ts
+++ app/_utils/firebaseInit.ts
@@ -11,8 +11,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
-console.log(firebaseConfig);
-
 initializeApp(firebaseConfig);
 
 // Connect database to emulator if running locally
