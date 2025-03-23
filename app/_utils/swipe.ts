55704aaedb5e2eb82ffc13ab8cbdf8b3e328f353
PlayXman <play.man.life@seznam.cz>
2025-03-23 22:27:54 +1100

    refactor: swipe-able elements to make them more performant

---
+++ app/_utils/swipe.ts
@@ -0,0 +1,6 @@
+/**
+ * The app runs on device supporting touch inputs.
+ */
+export function isTouchDevice(): boolean {
+  return window != null && ('ontouchstart' in window || navigator.maxTouchPoints > 0);
+}
